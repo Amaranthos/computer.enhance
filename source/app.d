cc5815aff22ebe84a60188f77a32608044b49108
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2023-03-08 15:41:05 +1000

    Init

---
+++ source/app.d
@@ -0,0 +1,15 @@
+import std.stdio;
+import std.getopt;
+import std.json;
+import config;
+
+import file = std.file;
+
+// config/command line variables
+string input;
+
+int main(string[] args)
+{
+
+	return 0;
+}
